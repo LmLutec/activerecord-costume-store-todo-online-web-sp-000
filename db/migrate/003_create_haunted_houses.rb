0c93b458fc34a780ff6988f3b5effe906a417c38
Lauren McCoy <lmccoy8911@gmail.com>
2019-07-16 22:53:49 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,7 +10,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.boolean :family_friendly
       t.text :opening_date
       t.text :closing_date
-      t.description :description
+      t.text :description
     end 
   end
   
