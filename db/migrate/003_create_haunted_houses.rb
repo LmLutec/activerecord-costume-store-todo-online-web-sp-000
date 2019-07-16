2e4065af1ac10a26e91c58af5d20288ae6581bff
Lauren McCoy <lmccoy8911@gmail.com>
2019-07-16 22:49:11 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses
+  
+  def change 
+    create_table :haunted_houses do |t|
+      t.string :name 
+      t.
+  
+  
+  
+  end
+  
+  
+end 
